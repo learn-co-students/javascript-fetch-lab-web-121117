82717324247ac8c39bb93270125013bd5b466bb1
github username <github email address>
2018-02-04 11:40:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-function getIssues() {
-}
+function getIssues() {}
 
-function showIssues(json) {
-}
+function showIssues(json) {}
 
-function createIssue() {
-}
+function createIssue() {}
 
 function showResults(json) {
+  return json;
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const repo = "learn-co-curriculum/javascript-fetch-lab";
+  const token = "be32af11ce6f68ecb1f5393927f50457bde70591";
+  const postData = {
+    body: "Great stuff"
+  };
+
+  fetch(`https://api.github.com/repos/${repo}`, {
+    method: "post",
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => console.log(res));
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return ''
+  return "";
 }
