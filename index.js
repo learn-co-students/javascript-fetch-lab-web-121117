70ec91336250fb31af2ac089cf72349347a22bbd
github username <github email address>
2018-02-04 12:16:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const token = "be32af11ce6f68ecb1f5393927f50457bde70591";
+const name = "k1k1272";
+const lab = "javascript-fetch-lab";
 function getIssues() {
-  fetch(`https://api.github.com/repos/k1k1272/javascript-fetch-lab/issues`, {
+  fetch(`https://api.github.com/repos/${name}/${lab}/issues`, {
     headers: {
       Authorization: `token ${token}`
     }
@@ -21,7 +23,7 @@ function createIssue() {
     title: document.getElementById("title").value,
     body: document.getElementById("body").value
   };
-  fetch(`https://api.github.com/repos/k1k1272/javascript-fetch-lab/issues`, {
+  fetch(`https://api.github.com/repos/${name}/${lab}/issues`, {
     method: "post",
     body: JSON.stringify(info),
     headers: {
@@ -37,15 +39,12 @@ function showResults(json) {
 }
 
 function forkRepo() {
-  fetch(
-    `https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab`,
-    {
-      method: "post",
-      headers: {
-        Authorization: `token ${getToken()}`
-      }
+  fetch(`https://api.github.com/repos/learn-co-curriculum/${lab}`, {
+    method: "post",
+    headers: {
+      Authorization: `token ${token}`
     }
-  )
+  })
     .then(res => res.json())
     .then(json => showResults(json));
 }
